a8ba6df789433408a9a55b68c3dd17d1bb7df1fa
Jose Cruz <cruzjosework@gmail.com>
2021-01-09 13:48:52 -0500

    Updated app.js with working form, can access value, on age, name, balance forms. have request form, but not finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [balance, setBalance] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form id="personal-form" onSubmit={(e)=> {
+        e.preventDefault();
+
+        console.log(name);
+        console.log(age);
+        console.log(balance);
+      }}>
+
+        <label htmlFor="name">Name </label>
+        <input type="text" name="name" onChange={(e) => {
+          setName(e.target.value )
+        }}></input>
+      
+        <label htmlFor="age">Age </label>
+        <input type="number" name="age" onChange={(e) => {
+          setAge(e.target.value)
+        }}></input>
+
+        <label htmlFor="balance">Balance </label>
+        <input type="number" name="balance" onChange={(e) => {
+          setBalance(e.target.value)
+        }}></input>
+        <button type="submit">send</button>
+      </form>
+
+      <form id="request">
+        <label htmlFor="customer-id">Customer ID</label>
+        <input type="number" name="customer-id"></input>
+        <button type="submit">request</button>
+      </form>
     </div>
   );
 }
