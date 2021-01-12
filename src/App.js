652f24108959792d1160069d4f60a1f7efb3397c
ashok997 <ashok997@gmail.com>
2021-01-12 08:56:50 -0600

    tested fetching single user info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
@@ -9,75 +9,86 @@ function App() {
 
   const updateNameOnChange = (e) => {
     setName(e.target.value);
-  }
+  };
 
   const updateAgeOnChange = (e) => {
     setAge(e.target.value);
-  }
+  };
 
   const updateBalanceOnChange = (e) => {
     setBalance(e.target.value);
-  }
+  };
 
   const updateCustomerIdOnChange = (e) => {
     setCustomerID(e.target.value);
-  }
-
+  };
 
   const postRequest = async (url, body) => {
-    const response = await fetch(url,{
+    const response = await fetch(url, {
       method: "POST",
-      headers:{
-        'content-type': 'application/json',
+      headers: {
+        "content-type": "application/json",
       },
-      body:JSON.stringify(body)
+      body: JSON.stringify(body),
+    });
 
-    })
-
-    if(!response.ok) {
-      console.log("error response is not okay")
+    if (!response.ok) {
+      console.log("error response is not okay");
     }
-    
-   }
+  };
 
   const onSubmitForm = (e) => {
-    //once button take state variables and send through api service 
+    //once button take state variables and send through api service
     e.preventDefault();
     const userInfo = {
       name,
       age,
-      balance
-    }
-    postRequest("http://localhost:8080", userInfo)
+      balance,
+    };
+    postRequest("http://localhost:8080", userInfo);
     // console.log(name);
     // console.log(age);
     // console.log(balance);
   };
 
+  const loadUser = async () => {
+    const resp = await fetch("http://localhost:8080/customer?customer-id=1");
+    const json = await resp.json();
+    console.log(json);
+  };
+
   const onRequestForm = (e) => {
-    //once button take state variables and send through api service 
+    //once button take state variables and send through api service
     e.preventDefault();
     console.log(customerID);
+    loadUser();
   };
 
   return (
     <div className="App">
-      <form id="personal-form" onSubmit={(e)=> onSubmitForm(e)}>
-
+      <form id="personal-form" onSubmit={(e) => onSubmitForm(e)}>
         <label htmlFor="name">Name </label>
         <input type="text" name="name" onChange={updateNameOnChange}></input>
-      
+
         <label htmlFor="age">Age </label>
         <input type="number" name="age" onChange={updateAgeOnChange}></input>
 
         <label htmlFor="balance">Balance </label>
-        <input type="number" name="balance" onChange={updateBalanceOnChange}></input>
+        <input
+          type="number"
+          name="balance"
+          onChange={updateBalanceOnChange}
+        ></input>
         <button type="submit">send</button>
       </form>
 
       <form id="request" onSubmit={(e) => onRequestForm(e)}>
         <label htmlFor="customerID">Customer ID</label>
-        <input type="number" name="customerID" onChange={updateCustomerIdOnChange}></input>
+        <input
+          type="number"
+          name="customerID"
+          onChange={updateCustomerIdOnChange}
+        ></input>
         <button type="submit">request</button>
       </form>
     </div>
